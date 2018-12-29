324ca38aa10ebc7298f8d2f6a5ce2af880c3e1fa
htasha <ahsath@gmail.com>
2018-12-29 00:48:29 -0400

    Update: set app state before enter

--- src/router.js
+++ src/router.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import store from '@/store';
+import {
+  FETCH_ALL_DOCUMENTS
+} from '@/store/action-types';
+import {
+  SET_STATE
+} from '@/store/mutation-types';
 
 Vue.use(Router)
 
@@ -10,6 +17,12 @@ export default new Router({
   routes: [{
     path: '/',
     name: 'home',
-    component: Home
+    component: Home,
+    beforeEnter: async (to, from, next) => {
+      let docs = await store.dispatch('clients/' + FETCH_ALL_DOCUMENTS)
+      let mappedItems = docs.rows.map(el => el.doc);
+      store.commit("clients/" + SET_STATE, mappedItems);
+      next()
+    }
   }]
 })
