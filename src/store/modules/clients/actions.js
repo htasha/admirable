91145b0a484c7a8964a6423eaa91b4bc33bb4542
htasha <ahsath@gmail.com>
2018-12-30 04:57:19 -0400

    Updated

--- src/store/modules/clients/actions.js
+++ src/store/modules/clients/actions.js
@@ -1,15 +1,42 @@
 import {
     CREATE_NEW_DOC,
-    FETCH_ALL_DOCUMENTS
+    FETCH_ALL_DOCUMENTS,
+    UPDATE_DOCUMENT
 } from '@/store/action-types';
 import {
-    ADD_NEW_DATABLE_ITEM
+    ADD_NEW_DATABLE_ITEM,
+    UPDATE_DATABLE_ITEM
 } from '@/store/mutation-types'
 import createNewDoc from '@/utils/models/create-new-doc'
 import fetchAllDocs from '@/utils/models/fetch-all-docs'
+import updateDoc from '@/utils/models/update-doc'
 
 export default {
-    async [FETCH_ALL_DOCUMENTS](context) {
+    async [UPDATE_DOCUMENT](context, payload) {
+        let selectedItems = context.state.dataTable.selectedDatableItem
+        let isSelected = selectedItems.length > 0
+        let docs = isSelected ? selectedItems.map(element => Object.assign({}, element, payload)) : [payload.update]
+
+        try {
+            let response = await updateDoc(docs)
+            if (!isSelected) {
+                let singleDoc = {
+                    oldDoc: payload.oldDoc,
+                    update: response[0]
+                }
+                context.commit(UPDATE_DATABLE_ITEM, singleDoc)
+            } else {
+                context.commit(UPDATE_DATABLE_ITEM, response)
+            }
+            return {
+                updated: true,
+                message: 'Registro(s) actualizado(s) exitosamente'
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    async [FETCH_ALL_DOCUMENTS]() {
         try {
             let docs = await fetchAllDocs()
             if (docs) return docs
@@ -24,7 +51,7 @@ export default {
                 context.commit(ADD_NEW_DATABLE_ITEM, item)
                 return {
                     created: true,
-                    message: 'Item created successfully'
+                    message: 'Registro creado exitosamente'
                 }
             }
         } catch (error) {
