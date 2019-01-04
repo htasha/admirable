c9c58c6965924e12a991a237bbd03b4e23310444
htasha <ahsath@gmail.com>
2019-01-04 04:08:19 -0400

    Create new index

--- src/utils/db/index.js
+++ src/utils/db/index.js
@@ -1,3 +1,16 @@
 import PouchDB from 'pouchdb'
+import PouchdbFind from 'pouchdb-find'
+PouchDB.plugin(PouchdbFind)
+
 // Create clients database
 export const Clients = new PouchDB('clients')
+
+// Create technicians database
+export const Technicians = new PouchDB('technicians')
+
+// Create index for look up technician field in Clients
+Clients.createIndex({
+    index: {
+        fields: ['technician']
+    }
+})
