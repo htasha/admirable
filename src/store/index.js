d29623a3d7a0ab1aedd8e4b33f67b3e2c2161ab5
htasha <ahsath@gmail.com>
2019-01-04 18:27:39 -0400

    Added simple persist plugin to the <store>

--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import clients from '@/store/modules/clients'
 import technicians from '@/store/modules/technicians'
+import VueSimplePersistPlugin from '@/plugins/vue-simple-persist'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  plugins: [VueSimplePersistPlugin],
   modules: {
     clients,
     technicians
