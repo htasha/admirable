d13aad96373e6bcf36bfcea9e48cd8cc3d88a001
htasha <ahsath@gmail.com>
2019-01-04 04:04:45 -0400

    Added <technicians> module to the vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import clients from '@/store/modules/clients'
+import technicians from '@/store/modules/technicians'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    clients
+    clients,
+    technicians
   }
 })
