b5d2aaa56732c69cd80cc125d6dfed739eb1d573
htasha <ahsath@gmail.com>
2019-01-04 18:30:18 -0400

    Added: enable dark mode option

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,14 +1,43 @@
 <template>
   <v-container>
     <v-layout>
-      <h1>Settings</h1>
+      <v-flex justify-center layout>
+        <v-card width="600">
+          <v-list dense>
+            <v-list-tile avatar>
+              <v-list-tile-avatar>
+                <v-icon>mdi-brightness-2</v-icon>
+              </v-list-tile-avatar>
+              <v-list-tile-content>
+                <v-list-tile-title class="subheading">Modo obscuro</v-list-tile-title>
+              </v-list-tile-content>
+              <v-list-tile-action>
+                <v-switch v-model="mode"></v-switch>
+              </v-list-tile-action>
+            </v-list-tile>
+          </v-list>
+        </v-card>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
-export default {};
+import { mapMutations, mapGetters } from "vuex";
+export default {
+  methods: {
+    ...mapMutations("clients", ["ENABLE_DARK"])
+  },
+  computed: {
+    ...mapGetters("clients", ["dark"]),
+    mode: {
+      get: function() {
+        return this.dark;
+      },
+      set: function(val) {
+        this.ENABLE_DARK(val);
+      }
+    }
+  }
+};
 </script>
-
-<style>
-</style>
