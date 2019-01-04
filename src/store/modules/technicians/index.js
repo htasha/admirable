a4a98a0e417b173107bf4ea479288d3a71c494ab
htasha <ahsath@gmail.com>
2019-01-04 04:06:45 -0400

    Added state object to <technicians> module

---
+++ src/store/modules/technicians/index.js
@@ -0,0 +1,20 @@
+import mutations from './mutations'
+import getters from './getters'
+import actions from './actions'
+
+export default {
+    namespaced: true,
+    state: {
+        technicians: [],
+        statusItems: [
+            "Listo para entregar",
+            "Esperando repuesto",
+            "En reparación",
+            "Retirado",
+            "Garantía"
+        ]
+    },
+    mutations,
+    getters,
+    actions
+}
