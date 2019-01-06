da3e23acb58a65e89b6b5970e79ddcaff02bb258
htasha <ahsath@gmail.com>
2019-01-05 21:11:13 -0400

    Updated: <ADD_NEW_TECHNICIAN> return obj

--- src/store/modules/technicians/actions.js
+++ src/store/modules/technicians/actions.js
@@ -4,25 +4,16 @@ import {
     GET_ALL_TECHNICIANS,
     DELETE_TECHNICIAN
 } from '@/store/action-types';
-import {
-    LOAD_TECHNICIANS_STATE
-} from '@/store/mutation-types';
-import {
 
-} from '@/store/mutation-types';
 import createNewDocument from '@/utils/technician-models/create-new-document'
 import findAllDocsByTechnicianName from '@/utils/technician-models/find-all-docs-by-technician-name'
 import fetchAllDocuments from '@/utils/technician-models/fetch-all-documents'
-import findTechnicianById from '@/utils/technician-models/find-technician-by-id'
 import deleteDocument from '@/utils/technician-models/delete-document'
 
 export default {
     async [TECHNICIAN_EXIST](context, name) {
         try {
             let result = await findAllDocsByTechnicianName(name)
-            if (result.found) Object.assign(result, {
-                message: 'Este nombre ya se encuentra registrado'
-            })
             return result
         } catch (error) {
             throw error
@@ -30,9 +21,11 @@ export default {
     },
     async [ADD_NEW_TECHNICIAN](context, technician) {
         try {
-            let docID = await createNewDocument(technician)
-            let doc = await findTechnicianById(docID)
-            return doc
+            let doc = await createNewDocument(technician)
+            return {
+                doc,
+                created: true
+            }
         } catch (error) {
             throw error
         }
