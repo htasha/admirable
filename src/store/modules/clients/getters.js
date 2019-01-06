eb96ff4c0110243fd7f5614bfeaecc0ac864452c
htasha <ahsath@gmail.com>
2019-01-05 21:07:25 -0400

    Added: <snackbar>, <snackbarMessage> & <getSelectedDatableItemLength> getter

--- src/store/modules/clients/getters.js
+++ src/store/modules/clients/getters.js
@@ -1,8 +1,11 @@
 export default {
     getDataTableItems: state => state.dataTable.dataTableItems,
     getDataTableItemToEdit: state => state.dataTable.dataTableItemToEdit,
+    getSelectedDatableItemLength: state => state.dataTable.selectedDatableItem.length,
     getSearch: state => state.dataTable.search,
     getTechnician: state => state.dataTable.filters.technician || '',
     getStatus: state => state.dataTable.filters.status || '',
-    dark: state => state.dark
+    dark: state => state.dark,
+    snackbar: state => state.snackbar,
+    snackbarMessage: state => state.snackbarMessage
 }
