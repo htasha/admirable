af0fcc13b2cb58005b14048fe9fc6c3c171ef008
htasha <ahsath@gmail.com>
2019-01-05 21:12:44 -0400

    Moved: <findTechnicianById> model to <createNewDocument> model

--- src/utils/technician-models/create-new-document.js
+++ src/utils/technician-models/create-new-document.js
@@ -1,11 +1,15 @@
 import {
     Technicians
 } from '../db'
+import findTechnicianById from '@/utils/technician-models/find-technician-by-id'
 
 export default async doc => {
     try {
         let result = await Technicians.put(doc)
-        if (result.ok) return result.id
+        if (result.ok) {
+            let doc = await findTechnicianById(result.id)
+            return doc
+        }
     } catch (error) {
         throw error
     }
