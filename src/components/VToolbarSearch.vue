a712e03dff97b7a06c1d9459d21c9f1d2055468a
htasha <ahsath@gmail.com>
2019-01-06 01:49:50 -0400

    Change toolbar color based on dark theme

--- src/components/VToolbarSearch.vue
+++ src/components/VToolbarSearch.vue
@@ -1,11 +1,11 @@
 <template>
-  <v-toolbar app clipped-left>
+  <v-toolbar app dark clipped-left :color="color">
     <v-toolbar-side-icon @click="IS_MINI"></v-toolbar-side-icon>
     <v-toolbar-title>Admirable</v-toolbar-title>
     <v-spacer></v-spacer>
     <v-text-field
       flat
-      solo
+      solo-inverted
       hide-details
       append-icon="mdi-magnify"
       placeholder="Busqueda"
@@ -17,11 +17,17 @@
 </template>
 
 <script>
-import { mapMutations } from "vuex";
+import { mapMutations, mapGetters } from "vuex";
 
 export default {
   methods: {
     ...mapMutations("clients", ["UPDATE_SEARCH", "IS_MINI"])
+  },
+  computed: {
+    ...mapGetters("clients", ["dark"]),
+    color() {
+      return this.dark ? "" : "primary";
+    }
   }
 };
 </script>
