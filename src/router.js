6be69b16a90597163d1d47e4b919b9e793e1e82c
htasha <ahsath@gmail.com>
2019-01-12 00:22:14 -0400

    Paths array updated to simple routes

--- src/router.js
+++ src/router.js
@@ -1,16 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 import Clients from '@/views/Clients.vue'
 import Company from '@/views/Company.vue'
 import Settings from '@/views/Settings.vue'
-import store from '@/store';
-import {
-  FETCH_ALL_DOCUMENTS
-} from '@/store/action-types';
-import {
-  SET_STATE
-} from '@/store/mutation-types';
 
 Vue.use(Router)
 
@@ -18,20 +10,16 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [{
-    path: '/',
-    component: Home,
-    children: [{
-        path: 'clients',
-        component: Clients
-      },
-      {
-        path: 'company',
-        component: Company
-      },
-      {
-        path: 'settings',
-        component: Settings
-      }
-    ]
-  }]
+      path: '/',
+      component: Clients
+    },
+    {
+      path: '/company',
+      component: Company
+    },
+    {
+      path: '/settings',
+      component: Settings
+    }
+  ]
 })
