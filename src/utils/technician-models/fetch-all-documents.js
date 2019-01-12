2a9dc6ef8d11c0b9b87ece2fa28bd50c9a7eb29b
htasha <ahsath@gmail.com>
2019-01-12 00:16:51 -0400

    Update: check if docs.rows has any documents otherwise return empty array

--- src/utils/technician-models/fetch-all-documents.js
+++ src/utils/technician-models/fetch-all-documents.js
@@ -7,7 +7,8 @@ export default async name => {
         let docs = await Technicians.allDocs({
             include_docs: true
         })
-        if (docs) return docs.rows.map(el => el.doc)
+        if (docs.rows.length > 0) return docs.rows.map(el => el.doc)
+        return []
     } catch (error) {
         throw error
     }
